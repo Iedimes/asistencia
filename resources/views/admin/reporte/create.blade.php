d57a4f787e224d75ca527f201bea77a9ae774c23
Iedimes <osemidei@gmail.com>
2022-08-17 15:02:12 -0400

    vista

--- resources/views/admin/reporte/create.blade.php
+++ resources/views/admin/reporte/create.blade.php
@@ -54,7 +54,7 @@
                     </div>
 
                     <div class="form-group row align-items-center" :class="{'has-danger': errors.has('user_id'), 'has-success': fields.user_id && fields.user_id.valid }">
-                        <label for="user_id" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-2'">Tecnico</label>
+                        <label for="user_id" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-2'">{{ trans('admin.reporte.columns.user_id') }}</label>
                             <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-8'">
                             {{-- <input type="text" v-model="form.user_id"  @input="validate($event)" class="form-control" :class="{'form-control-danger': errors.has('user_id'), 'form-control-success': fields.user_id && fields.user_id.valid}" id="user_id" name="user_id" placeholder="{{ trans('admin.reporte.columns.user_id') }}"> --}}
                             <select name="user_id" id="user_id" v-model="form.user_id" class="form-control">
