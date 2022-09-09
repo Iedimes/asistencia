1f6429eb44231184aaa96827ea2b9f02103b1d7d
Iedimes <osemidei@gmail.com>
2022-09-09 08:49:11 -0400

    se comenta full name

--- resources/views/admin/help/finalizadas.blade.php
+++ resources/views/admin/help/finalizadas.blade.php
@@ -93,7 +93,7 @@
                                         <td>@{{ item.fone }}</td>
                                         <td>@{{ item.problem }}</td>
                                         <td class="text-center"><span :class="item.statuses.state.name == 'FINALIZADO' ? 'badge bg-primary' : 'badge bg-warning' ">@{{  item.statuses.state.name}}</span></td>
-                                        <td>@{{ item.statuses.user.full_name }}</td>
+                                        {{-- <td>@{{ item.statuses.user.full_name }}</td> --}}
                                         <td>@{{ item.tecnico.updated_at | datetime }}</td>
 
                                         <td>
